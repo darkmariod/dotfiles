c7db4c0e470d76a0511ea1fcfdacc025a971fbb6
Mario <109123293+darkmariod@users.noreply.github.com>
2025-04-21 09:33:42 -0500

    updated
    Oil nvim
    Shortcuts

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,8 @@
 return {
     "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- opcional
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-        view_options = {
-          show_hidden = true,
-        },
-        keymaps = {
-          ["<C-h>"] = "actions.parent",  -- ir al directorio padre
-          ["<C-s>"] = false,             -- desactiva keymap si no lo usas
-        },
-      })
-    end,
+      require("oil").setup({})
+    end
   }
   
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,12 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("telescope").setup({})
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+    
+    telescope.setup({})
+
+    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
   end
 }
